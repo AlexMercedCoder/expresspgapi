4cda7cd3a10b015d0a238f1bda15dc4c53d00082
Alex Merced <willcodeforfood@alexmerced.com>
2020-10-23 09:53:56 -0400

    cors update

--- configs/cors.js
+++ configs/cors.js
@@ -1,12 +1,13 @@
-var whitelist = ['http://example1.com', 'http://example2.com']
+//Replace the asterik with frontend url once frontend deployed.
+var whitelist = ["*"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error("Not allowed by CORS"));
     }
-  }
-}
+  },
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
